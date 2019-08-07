821e29b14ee0e1d93bb949724813bef4352e5af7
Jarosław Pawlak <jaroslaw.pawlak@gmail.com>
2019-08-07 20:59:16 +0200

    update

--- FoodLog.js
+++ FoodLog.js
@@ -1,6 +1,8 @@
 "use strict";
 const api_key = "b76d77635da6f609d5e8e84c1bca496470889617492391c87f75afc283e97ad4";
 
+window.addEventListener('cloudkitloaded', function() {})
+
 CloudKit.configure({
     containers: [{
         containerIdentifier: 'iCloud.pl.majatech.FoodLog',
