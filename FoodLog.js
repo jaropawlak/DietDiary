c043e7f5a887e1e3723239959dcb88fcde0c296e
Jarosław Pawlak <jaroslaw.pawlak@gmail.com>
2019-09-02 06:53:08 +0200

    draft

--- FoodLog.js
+++ FoodLog.js
@@ -9,114 +9,119 @@ window.addEventListener('cloudkitloaded', function() {
             apiTokenAuth: {
                 apiToken: api_key,
                 persist: true,
-
                 signInButton: {
                     id:"apple-sign-in-button"
                 },
-
                 signOutButton: {
                     id:"apple-sign-out-button"
-                }
-                
+                }                
             },
             environment: 'development'
         }]
     });
     console.log("Cloudkit configured");
+    
     function FoodWebModel() {
         var self = this;
         console.log("get containers");
         let container = CloudKit.getDefaultContainer();
         let database = container.privateCloudDatabase;
-        self.categories = ko.observableArray();
-        self.items = ko.observableArray();
-        self.getCategoryName = function(recordName) {
-            if (self.categoriesReference)
-            {
-                var cat = self.categoriesReference.find(function(element) {
-                    return element.recordName === recordName;
-                });
-                if (cat) {
-                    return cat.fields.name.value;
-                }
-                return "N/A";
-            }
-        }
-        self.fetchCategories = function() {
-            console.log("fetching categories from " + database);
-            var query = {recordType: 'Category'}; // sortBy: [{fieldName: '???'}]
-            return database.performQuery(query).then(function(response){
-                if (response.hasErrors) {
-                    console.error(response.errors[0]);
-                    return;
-                }
-                var records = response.records;
-                console.log(records);
-                var numberOfRecords = records.length;
-                if (numberOfRecords === 0) {
-                    console.error("No matching items");
-                    return;
-                }
-                self.categoriesReference = records;
-                self.categories(records);
-            })
-        }
-        self.fetchItems = function() {
-            console.log("fetching items from " + database);
-            var query = {recordType: 'LogItem'}; // sortBy: [{fieldName: '???'}]
-            return database.performQuery(query).then(function(response){
-                if (response.hasErrors) {
-                    console.error(response.errors[0]);
-                    return;
-                }
-                var records = response.records;
-                
-                //console.log(records);
-                var numberOfRecords = records.length;
-                if (numberOfRecords === 0) {
-                    console.error("No matching items");
-                    return;
-                }
-                records.forEach(function(element) {
-                    console.log(element)
-                    if (element.fields.category) {
-                        element.fields.categoryName = self.getCategoryName(element.fields.category.value.recordName);
-                    }
-                });
-                self.items(records);
-            })
-        }
+        self.categories = [];
+        self.items = [];
         container.setUpAuth().then(function(userInfo) {
             console.log("setUpAuth");
             if(userInfo) {
                 self.gotoAuthenticatedState(userInfo);
-                self.fetchCategories();
-                self.fetchItems();
+                
                } else {
                 self.gotoUnauthenticatedState();
                }        
         })
-       
-        self.displayUserName = ko.observable('Unauthenticated User');
         self.gotoAuthenticatedState = function(userInfo) {
-        if(userInfo.isDiscoverable) {
-            self.displayUserName(userInfo.firstName + ' ' + userInfo.lastName);
-        } else {
-            self.displayUserName('User Who Must Not Be Named');
-        }
-        container.whenUserSignsOut().then(self.gotoUnauthenticatedState);
+            document.getElementById("ForLoggedIn").style.display = "block";
+
+
+            container.whenUserSignsOut().then(self.gotoUnauthenticatedState);
         };
         
         self.gotoUnauthenticatedState = function(error) {
-            self.displayUserName('Unauthenticated User');          
+            document.getElementById("ForLoggedIn").style.display = "none";
             container
             .whenUserSignsIn()
             .then(self.gotoAuthenticatedState)
             .catch(self.gotoUnauthenticatedState);
           };
     }
-    ko.applyBindings(new FoodWebModel());
-   
-
-    
+        
+        
+     
 })
+
+class DataAccess {
+    async fetchCategories() {
+        
+        
+            var query = {recordType: 'Category'}; // sortBy: [{fieldName: '???'}]
+            let response =  await database.performQuery(query);
+            
+            if (response.hasErrors) {
+                console.error(response.errors[0]);
+                return;
+            }
+            var records = response.records;
+            console.log(records);
+            var numberOfRecords = records.length;
+            if (numberOfRecords === 0) {
+                console.error("No matching items");
+                return;
+            }
+            this.categoryList = records;
+            return records;
+    }
+
+    get categories() {
+        return this.categoryList;
+    } 
+
+    async fetchItems() {
+        let container = CloudKit.getDefaultContainer();
+        let database = container.privateCloudDatabase;
+        console.log("fetching items from " + database);
+        var query = {recordType: 'LogItem'}; // sortBy: [{fieldName: '???'}]
+        let response = await database.performQuery(query);
+        
+        if (response.hasErrors) {
+            console.error(response.errors[0]);
+            return;
+        }
+        var records = response.records;
+        
+        var numberOfRecords = records.length;
+        if (numberOfRecords === 0) {
+            console.error("No matching items");
+            return;
+        }
+        records.forEach(function(element) {
+            console.log(element)
+            if (element.fields.category) {
+                element.fields.categoryName = this.getCategoryName(element.fields.category.value.recordName);
+            }
+        });
+        this.items = records;
+        
+      
+    }
+
+    self.getCategoryName = function(recordName) {
+        if (self.categoriesReference)
+        {
+            var cat = self.categoriesReference.find(function(element) {
+                return element.recordName === recordName;
+            });
+            if (cat) {
+                return cat.fields.name.value;
+            }
+            return "N/A";
+        }
+    }
+}
